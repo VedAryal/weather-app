fc9ded469a2c116abf7f8b193a624707d27ae8d9
Ved Aryal <aryalved1@gmail.com>
2019-10-22 17:10:31 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { temp: 0, desc: "", icon: "", loading: true };
+  }
+  componentDidMount() {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=6fd138d7824444ac43192603e7cd897c"
+    )
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({
+          temp: responseData.main.temp,
+          desc: responseData.weather[0].description,
+          icon: responseData.weather[0].icon,
+          loading: false
+        });
+      })
+      .catch(err => console.error(err));
+  }
   render() {
-    return <div className="App"></div>;
+    const imgSrc = `http://openweathermap.org/img/w/${this.state.icon}.png`;
+    if (this.state.loading) {
+      return <p>Loading</p>;
+    } else {
+      return (
+        <div className="App">
+          <p>Temprature: {this.state.temp} °C</p>
+          <p>Description: {this.state.desc}</p>
+          <img src={imgSrc} alt="weather icon" />
+        </div>
+      );
+    }
   }
 }
 export default App;
